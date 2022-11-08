55ac4de126d7d786535692e550f4a4e69dc7910b
spacegray186 <109955351+spacegray186@users.noreply.github.com>
2022-11-08 18:05:06 +0900

    Day071_221108

---
+++ src/main/java/net/mem/MemDTO.java
@@ -0,0 +1,56 @@
+package net.mem;
+
+public class MemDTO {
+
+	private int num;
+    private String name;
+    private int age;
+    
+    public MemDTO() {}
+    
+    public MemDTO(int num) {
+    	this.num = num;
+    }
+    
+    public MemDTO(int num, int age) {
+    	this.num = num;
+    	this.age = age;
+    }
+    
+    public MemDTO(String name, int age) {
+    	this.name = name;
+    	this.age = age;
+    }
+    
+    public MemDTO(int num, String name, int age) {
+    	this.num = num;
+    	this.name = name;
+    	this.age = age;
+    }
+    
+
+	public int getNum() {
+		return num;
+	}
+
+	public void setNum(int num) {
+		this.num = num;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+    
+}//class end
